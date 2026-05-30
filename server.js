b9a54764466f57cd2dfaff565c9eefe18356af48
seungmincho72 <seungmincho72@gmail.com>
2026-05-31 00:53:27 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "insta-auto-backend",
+  "version": "1.0.0",
+  "description": "Instagram Auto DM Backend Server",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.8",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2"
+  }
+}
